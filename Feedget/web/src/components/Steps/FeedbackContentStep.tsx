3ed1964bd759b630f2c7fb4060ccfbb5de1f2de2
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-06 16:38:23 -0300

    feat(Feedget/web): Stage 2 - create ScreenshotButton component

--- Feedget/web/src/components/Steps/FeedbackContentStep.tsx
+++ Feedget/web/src/components/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { CloseButton } from "../CloseButton";
 import { FeedbackType, feedbackTypes } from "../WidgetForm";
+import { ScreenshotButton } from "../WidgetForm/ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -8,7 +10,9 @@ interface FeedbackContentStepProps {
 }
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
-  const feedbackTypeInfo = feedbackTypes[feedbackType]
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
   return (
     <>
       <header>
@@ -33,13 +37,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6 text-zinc-100"/>
-          </button>
-
+          <ScreenshotButton onScreenshotTook={setScreenshot} screenshot={screenshot}/>
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
