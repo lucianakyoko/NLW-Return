c518cdf49a3565fc278d4b39715f3ee51b169526
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-07 15:17:48 -0300

    feat(Feedget/server): Stage 3 - Create a new feedback

--- Feedget/server/src/server.ts
+++ Feedget/server/src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
+app.use(express.json());
+
+app.post('/feedbacks', (req, res) => {
+  prisma.feedback.create({
+    data: {
+      type: req.body.type,
+      comment: req.body.comment,
+      screenshot: req.body.screnshot,
+    }
+  })
+
   return res.send('Hello, World!')
 })
 
